b1dc0adcee2445eb8484b58f65fa88486354370d
Yoandry Collazo <ycbarrosocu@gmail.com>
2021-10-18 16:46:33 -0400

    feat: linters configuration

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: { 'react/prop-types': 'off', 'react/react-in-jsx-scope': 'off' },
+}
