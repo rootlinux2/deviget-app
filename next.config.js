9cf0e6295812b83ed1070dd109ae4c0910dad3b8
Yoandry Collazo <ycbarrosocu@gmail.com>
2021-10-19 12:10:05 -0400

    fix: remove mxd

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  webpack: (config, { defaultLoaders }) => {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [defaultLoaders.babel, '@mdx-js/loader'],
+    })
+
+    return config
+  },
 }
